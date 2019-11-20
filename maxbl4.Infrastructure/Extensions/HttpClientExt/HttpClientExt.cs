139e9f76f296329c5eca13defb660a20027053cf
Max <maxbl4@gmail.com>
2019-11-20 22:06:38 +0300

    Revert a hack

--- maxbl4.Infrastructure/Extensions/HttpClientExt/HttpClientExt.cs
+++ maxbl4.Infrastructure/Extensions/HttpClientExt/HttpClientExt.cs
@@ -9,8 +9,6 @@ namespace maxbl4.Infrastructure.Extensions.HttpClientExt
         public static async Task<T> GetAsync<T>(this HttpClient client, string uri)
         {
             var response = await client.GetAsync(uri);
-            if (typeof(T) == typeof(string))
-                return (T)(object)await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
         }
     }
