ed3d4f7adcbf53e780d0af11ce5a2264dd91c2cf
Max <maxbl4@gmail.com>
2019-11-20 21:48:12 +0300

    HttpClient ext

---
+++ maxbl4.Infrastructure/Extensions/HttpClientExt/HttpClientExt.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace maxbl4.Infrastructure.Extensions.HttpClientExt
+{
+    public static class HttpClientExt
+    {
+        public static async Task<T> GetAsync<T>(this HttpClient client, string uri)
+        {
+            var response = await client.GetAsync(uri);
+            if (typeof(T) == typeof(string))
+                return (T)(object)await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
