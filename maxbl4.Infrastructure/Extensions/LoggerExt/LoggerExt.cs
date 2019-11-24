783181f22c78d72c2cb9cb45e6f4c1c54bd95b37
Max <maxbl4@gmail.com>
2019-11-24 23:23:10 +0300

    Remove Safe class in favor of extending LoggerExt.Swallow

--- maxbl4.Infrastructure/Extensions/LoggerExt/LoggerExt.cs
+++ maxbl4.Infrastructure/Extensions/LoggerExt/LoggerExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Serilog;
 
 namespace maxbl4.Infrastructure.Extensions.LoggerExt
@@ -13,7 +14,47 @@ namespace maxbl4.Infrastructure.Extensions.LoggerExt
             }
             catch (Exception ex)
             {
-                logger.Error("Error swallowed: {ex}", ex);
+                logger.Warning(ex, "Warning swallowed");
+            }
+        }
+        
+        public static Task Swallow(this ILogger logger, Func<Task> func)
+        {
+            try
+            {
+                return func().ContinueWith(x => logger.Warning(x.Exception, "Warning swallowed"),
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
+            catch (Exception ex)
+            {
+                logger.Warning(ex, "Warning swallowed");
+                return Task.CompletedTask;
+            }
+        }
+        
+        public static Task SwallowError(this ILogger logger, Func<Task> func)
+        {
+            try
+            {
+                return func().ContinueWith(x => logger.Error(x.Exception, "Error swallowed"),
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error swallowed");
+                return Task.CompletedTask;
+            }
+        }
+        
+        public static void SwallowError(this ILogger logger, Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error swallowed");
             }
         }
     }
