72519961cd9c59b64750e2ee237b4c79fd583516
Max <maxbl4@gmail.com>
2019-11-28 23:39:24 +0300

    RollingFile

---
+++ maxbl4.Infrastructure.Tests/RollingFileTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Shouldly;
+using Xunit;
+
+namespace maxbl4.Infrastructure.Tests
+{
+    public class RollingFileTests
+    {
+        [Fact]
+        public void Should_return_original_for_non_existing()
+        {
+            RollingFile.GetNext("aaa").ShouldBe("aaa");
+            RollingFile.GetCurrent("aaa").ShouldBe("aaa");
+            var full = Path.GetFullPath("aaa");
+            full.Length.ShouldBeGreaterThan(3);
+            RollingFile.GetNext(full).ShouldBe(full);
+        }
+        
+        [Fact]
+        public void Should_return_next()
+        {
+            File.WriteAllText("bb.txt", "text");
+            RollingFile.GetNext("bb.txt").ShouldBe("bb_001.txt");
+            RollingFile.GetCurrent("bb.txt").ShouldBe("bb.txt");
+        }
+        
+        [Fact]
+        public void Should_return_next_next()
+        {
+            File.WriteAllText("cc.txt", "text");
+            File.WriteAllText("cc_001.txt", "text");
+            RollingFile.GetNext("cc.txt").ShouldBe("cc_002.txt");
+            RollingFile.GetCurrent("cc.txt").ShouldBe("cc_001.txt");
+        }
+        
+        [Fact]
+        public void Should_return_next_power()
+        {
+            File.WriteAllText("dd.txt", "text");
+            for (int i = 1; i < 10; i++)
+            {
+                File.WriteAllText($"dd_{i}.txt", "text");
+            }
+            RollingFile.GetNext("dd.txt", 1).ShouldBe("dd_01.txt");
+            RollingFile.GetCurrent("dd.txt", 1).ShouldBe("dd_9.txt");
+        }
+    }
+}
---
+++ maxbl4.Infrastructure/RollingFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace maxbl4.Infrastructure
+{
+    public static class RollingFile
+    {
+        private static string Find(string originalFilename, int numberOfDigits, bool returnExisting)
+        {
+            if (!File.Exists(originalFilename)) return originalFilename;
+            var dir = Path.GetDirectoryName(originalFilename) ?? "";
+            var ext = Path.GetExtension(originalFilename) ?? "";
+            var name = Path.GetFileNameWithoutExtension(originalFilename) ?? "";
+            var prevName = originalFilename;
+            for (var n = numberOfDigits; n < 10; n++)
+            {
+                for (var i = 1; i < Math.Pow(10, n); i++)
+                {
+                    var num = i.ToString(new string('0', n));
+                    var nextName = Path.Combine(dir, $"{name}_{num}{ext}");
+                    if (!File.Exists(nextName))
+                        return returnExisting ? prevName : nextName;
+                    prevName = nextName;
+                }
+            }
+
+            throw new IndexOutOfRangeException("Could not file free file name"); 
+        }
+
+        /// <summary>
+        /// Returns next rolling file name.
+        /// E.g my.log > my_001.log; my_005.log > my_006.log
+        /// </summary>
+        /// <param name="originalFilename"></param>
+        /// <param name="numberOfDigits"></param>
+        /// <returns></returns>
+        public static string GetNext(string originalFilename, int numberOfDigits = 3)
+        {
+            return Find(originalFilename, numberOfDigits, false);
+        }
+
+        public static string GetCurrent(string originalFilename, int numberOfDigits = 3)
+        {
+            return Find(originalFilename, numberOfDigits, true);
+        }
+    }
+}
